997438184ce7c73a0cc6ebd2d35d54d150c6d858
Jordan Bulpitt <Jordan.Bulpitt@tpnmail.co.uk>
2021-11-09 00:35:52 +0000

    dd
    dd

--- LuminareWebsite/src/app/app-routing.module.ts
+++ LuminareWebsite/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+//Component Imports
+import { HomeComponent} from '../app/modules/pages/home/home.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
